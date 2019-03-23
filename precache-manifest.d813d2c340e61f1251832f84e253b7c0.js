27a340ba72778190ea630192b81d81098570ad55
vikashkumr <vky0018@gmail.com>
2019-03-23 10:35:23 +0530

    Updates

---
+++ precache-manifest.d813d2c340e61f1251832f84e253b7c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robo-book/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "f5186a6fdbc9f275d1e4",
+    "url": "/robo-book/static/js/runtime~main.f5186a6f.js"
+  },
+  {
+    "revision": "746e56e48a4f76150fb9",
+    "url": "/robo-book/static/js/main.746e56e4.chunk.js"
+  },
+  {
+    "revision": "0f0f1ed395de9ee32ca5",
+    "url": "/robo-book/static/js/2.0f0f1ed3.chunk.js"
+  },
+  {
+    "revision": "746e56e48a4f76150fb9",
+    "url": "/robo-book/static/css/main.ba77e865.chunk.css"
+  },
+  {
+    "revision": "30431fcfe9c5b8a1bbe3db24b27e7d67",
+    "url": "/robo-book/index.html"
+  }
+];
